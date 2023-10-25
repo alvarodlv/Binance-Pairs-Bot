fb47f6a0770bbea6fd25794ceaf48f7a53630f73
alvarodlv123 <92057407+alvarodlv123@users.noreply.github.com>
2023-10-25 21:25:47 +0100

    adding logic

--- main.py
+++ main.py
@@ -5,12 +5,27 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import coint
 
 
-# Initiate API object
+# Download close prices for symbols
+#prices, returns = API.get_close_prices(client,symbols=['DOGEUSDT','WAVESUSDT'],interval="1h",start="2023-09-24")
+
+
+# Connect to Binance
 API = BinanceAPICalls(False)
 client = API.api_login()
 
-# Download close prices for symbols
-prices, returns = API.get_close_prices(client,symbols=['DOGEUSDT','WAVESUSDT'],interval="1h",start="2023-09-24")
+# Place market order to open/close positons
 
-plt.plot(prices.index, prices)
-plt.show()
+
+# Abort all open orders
+
+
+# Construct market prices to build table
+
+
+# Store cointegrated pairs 
+
+
+# While TRUE:
+# Manage existing trades
+
+# Open positions
