33ab0c3fcdb8c48b6bd02cc6f6b40cd7b739b056
alvarodlv123 <92057407+alvarodlv123@users.noreply.github.com>
2023-10-26 22:38:11 +0100

    refactor

---
+++ program/main.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+from program.binanceAPICalls import BinanceAPICalls
+from program.functions import print_json
+from statsmodels.tsa.stattools import coint
+from binance.enums import *
+from program.constants import *
+
+
+# Download close prices for symbols
+#prices, returns = API.get_close_prices(client,symbols=['DOGEUSDT','WAVESUSDT'],interval="1h",start="2023-09-24")
+
+
+# Connect to Binance
+API = BinanceAPICalls(True if MODE == 'TEST' else False)
+client = API.api_login()
+
+# Place market order to open/close positons
+
+
+# Abort all open orders
+if ABORT_ALL_POSITIONS:
+    close_orders = API.abort_all_positions(client)
+
+# Construct market prices to build table
+
+
+# Store cointegrated pairs 
+
+
+# While TRUE:
+# Manage existing trades
+
+# Open positions
